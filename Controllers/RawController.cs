02ca9bcf72c8ced7ca84ed8bd823c73a28357896
Min Naing Tun <minnaingtun.pro@gmail.com>
2025-11-26 15:03:59 +0630

    Setup for initial load system.

---
+++ Controllers/RawController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SIM_System.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RawController : ControllerBase
+    {
+        ILogger<RawController> _logger;
+        public RawController(ILogger<RawController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRawData()
+        {
+            var rawData = new List<object>
+            {
+                new
+                {
+                    id = 1,
+                    name = "John Doe",
+                    email = "9yK8d@example.com",
+                },
+                new
+                {
+                    id = 2,
+                    name = "Jack Willian",
+                    email = "t2s6W@example.com",
+                },
+                new
+                {
+                    id = 3,
+                    name = "Bob Smith",
+                    email = "t2s6W@example.com",
+                },
+                new
+                {
+                    id = 4,
+                    name = "Alice Johnson",
+                    email = "t2s6W@example.com",
+                }
+            };
+
+            return Ok(rawData);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using SIM_System.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Connectin string
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+//Dependency Injection
+
+//API optimization
+builder.Services.AddMemoryCache();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddResponseCompression();
+
+//Swagger
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -12,8 +26,30 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "/swagger/{documentname}/swagger.json";
+    });
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint($"/swagger/v1/swagger.json", "MBL Shipping Import Management System API Services");
+    });
+    app.UseDeveloperExceptionPage();
+}
+else if (app.Environment.IsStaging() || app.Environment.IsProduction())
+{
+    app.UseSwagger(c => {
+        c.RouteTemplate = "/swagger/{documentname}/swagger.json";
+        c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+        {
+            swaggerDoc.Servers = new List<OpenApiServer> { new OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}" } };
+        });
+    });
+
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint($"/swagger/v1/swagger.json", "MBL Shipping Import Management System API Services");
+    });
 }
 
 app.UseHttpsRedirection();
